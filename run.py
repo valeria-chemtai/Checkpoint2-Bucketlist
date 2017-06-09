35c02bbdfc6abbf281d3f0c2d8df9f3fd02be4b8
Valeria Chemtai <valeriachemtai28@gmail.com>
2017-06-09 15:28:42 +0300

    [feature]Add authentication to item operations

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 
 from app.views import create_app
 
-config_name = os.getenv('APP_SETTINGS')  # config_name = "development"
+config_name = "development"   # config_name = "development"
 app = create_app(config_name)
 
 if __name__ == '__main__':
