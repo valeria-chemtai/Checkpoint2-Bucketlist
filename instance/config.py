2dd6e4ff1675e3584190c7343ec5a00692bf75c6
Valeria Chemtai <valeriachemtai28@gmail.com>
2017-06-12 14:04:45 +0300

    [chore]Edit travis

--- instance/config.py
+++ instance/config.py
@@ -20,7 +20,7 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
     """Configurations for Testing, with a separate test database."""
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = "postgres:///test_db"
+    SQLALCHEMY_DATABASE_URI = "postgres://test_db"
     DEBUG = True
 
 
